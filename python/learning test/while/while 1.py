81fd58b7b31246e61b47a11b4547d2e878c20051
Sebastien Laroche <mr.sebas@hotmail.com>
2023-08-27 19:23:22 -0400

    learning test 2

---
+++ python/learning test/while/while 1.py
@@ -0,0 +1,12 @@
+
+# Ceci est un exercice du livre Apprendre a programmer avec python 3 de Gerard Swinnen
+
+# Exercice 4.2
+# Ecriver un programme qui affiche les 20 premiers termes de la table de multiplication
+# par 7.
+
+i = 0
+while i < 20:
+    print(i * 7)
+    i = i + 1
+
